93eb6ee16fb34fff42d35846ee3f3d2ce93f973f
Kacper Żurawik <kaacper181@gmail.com>
2022-12-05 19:55:47 +0100

    Commiting

---
+++ SimpleConsoleGame/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleConsoleGame
+{
+    class Program
+    {
+
+        int Height = 20;
+        int Width = 30;
+        
+        public int WriteBoard()
+        {
+            Console.Clear();
+            for (int i = 1; i <= (Width+2);i++)
+            {
+                Console.SetCursorPosition(i, 1);
+                Console.Write("-");
+            }    
+            Console.Clear();
+            for (int i = 1; i <= (Width+2);i++)
+            {
+                Console.SetCursorPosition(i, );
+                Console.Write("-");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+        
+        
+        }
+    }
+}
